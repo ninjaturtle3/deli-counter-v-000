8f75bb623b031c9037cd8a9e3f9b027aad0a0734
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-19 20:47:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,44 @@
-# Write your code here.
+#at the start of the day, there aren't any customers, the queue is empty
+#the empty queue will be represented by an empty array
+katz_deli = []
+
+#build the line method showing everyone their current place in line
+# if nobody is in line puts "The line is currently empty."
+def line(deli_lines)
+  if deli_lines.length == 0
+     puts "The line is currently empty."
+   else
+     deli_lines.each.with_index(1) do |name, index|
+       katz_deli.push("#{index}. #{name}")
+     end
+     puts "The line is currently: #{katz_deli.join(" ")}"
+   end
+ end
+
+#build the take_a_number method for new customers entering the deli
+
+
+
+##the take_a_number methods takes two arguments -- new costumer will use whenentering the deli
+          ###array for current people in line (katz_deli)
+          ###and a string containingthe name of the person joining the end of the line
+def take_a_number(katz_deli, name)
+  #the take_a_number method should puts the person's name along with their postion in line --use .push(element) to add a person's name to the line
+  katz_deli.push(name)
+
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+
+#build now_serving method
+def now_serving(katz_deli)
+  ##if nobody's in line, puts "There is nobody waiting to be served!"
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{(katz_deli[0])}."
+     katz_deli.shift()
+  end
+  ##it puts the next person in line and then remove them from the front --will use the .shift method to remove the first element in the array
+
+end
