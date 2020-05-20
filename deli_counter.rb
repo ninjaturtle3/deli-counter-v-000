4bce2dc270adc6d3027c727c4e1b8a38a989203e
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-20 20:04:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
    else
      current_line = []
      katz_deli.each.with_index(1) do |name,index|
-       katz_deli << "#{index}. #{name})"
+       current_line << "#{index}. #{name})"
        end
      puts "The line is currently: #{current_line.join(" ")}"
 
